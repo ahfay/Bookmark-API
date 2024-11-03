69f039fa029d618b0300bf8debc0be1e89bc743c
ahfay <122598374+ahfay@users.noreply.github.com>
2024-11-03 11:08:28 +0700

    Delete tests directory

--- tests/test.py
+++
@@ -1,9 +0,0 @@
-from sqids import Sqids
-
-def generate_short_url(url: str) -> str:
-    sqids = Sqids(min_length=3)
-    return sqids.encode(url)
-
-if __name__=='__main__':
-    a = generate_short_url("https://github.com/sqids/sqids-python")
-    print(a)
